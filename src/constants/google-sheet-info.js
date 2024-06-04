9f846dfa5a86a5a1d0f41e1202a2100f1dc30c43
Tung Thanh <tung32188@gmail.com>
2024-06-04 21:10:44 +0700

    add googleapi

--- src/constants/google-sheet-info.js
+++ src/constants/google-sheet-info.js
@@ -1,7 +1,7 @@
 export const GoogleSheetInfo = {
-    serviceAccountEmail: process.env.REACT_APP_SERVICE_ACCOUNT_EMAIL,
-    privateKey: process.env.REACT_APP_PRIVATE_KEY,
-    spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID,
-    clientId: process.env.REACT_APP_CLIENT_ID,
-    apiKey: process.env.REACT_APP_API_KEY,
+  serviceAccountEmail: process.env.REACT_APP_SERVICE_ACCOUNT_EMAIL,
+  privateKey: process.env.REACT_APP_PRIVATE_KEY,
+  spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID,
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
 };
