6e582fa3b14bbcb6a5f598dc1be607646ec2dd20
Tung Thanh <tung32188@gmail.com>
2024-06-05 11:18:54 +0700

    init app

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,11 +1,24 @@
 // src/pages/NotFound.js
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom if you're using React Router
+
 const NotFound = () => {
-    return (
-      <div>
-        <h1>Trang không tồn tại</h1>
-      </div>
-    );
-  };
-  
-  export default NotFound;
-  
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <img
+        src="https://sitechecker.pro/wp-content/uploads/2023/06/404-status-code.png"
+        alt="Page not found"
+        className="mb-8"
+      />
+      <h1 className="text-4xl mb-2">404 - Trang không tồn tại</h1>
+      <p className="text-lg mb-4">
+        Xin lỗi, trang bạn đang tìm kiếm không tồn tại.
+      </p>
+      <Link to="/" className="text-blue-500 hover:underline">
+        Trở về trang chủ
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
